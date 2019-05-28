d3c0bbd2f491b519bfff80c5f4dae9afd9895e6a
alessandrojr1998 <alessandrojr1998@gmail.com>
2019-05-28 01:01:04 -0300

    merge com o master principal feito e com o inicio do segundo cenario

---
+++ barbeariaWeb/features/criar_novo_endereco.feature
@@ -0,0 +1,8 @@
+Feature: Criar um novo endereco
+  Como administrador, quero criar um novo endereco
+
+  Scenario: Criar um endereco
+    Given Estou na pagina de novo endereco
+    When Eu crio um endereco com 'rua','bairro','numero' e 'id'
+    And Clico em criar endereco
+    Then Eu vejo uma tela com a mensagem de sucesso e os dados do endereco.
--- barbeariaWeb/features/step_definitions/stepdefs.rb
+++ barbeariaWeb/features/step_definitions/stepdefs.rb
@@ -15,6 +15,31 @@ When("Clico em criar usuario") do
   click_button 'Create Usuario'
 end
 
-Then("Eu vejo uma tela com a mensagem de sucesso e meus dados de novo cliente.") do
+Then("Eu vejo uma tela com a mensagem de sucesso e meus dados de novo usuario.") do
   page.has_no_content?('Usuario was successfully created.')
 end
+
+
+
+
+Given("Estou na pagina de novo endereco") do
+  visit '/enderecos/new'
+  expect(page).to have_current_path('/enderecos/new')
+end
+
+When("Eu crio um endereco com {string},{string},{string} e {string}") do |rua, bairro, numero, id|
+  fill_in 'endereco[rua]', :with => rua
+  fill_in 'endereco[bairro]', :with => bairro
+  fill_in 'endereco[numero]', :with => numero
+  fill_in 'endereco[id]', :with => id
+end
+
+When("Clico em criar endereco") do
+  click_button 'Create Endereco'
+end
+
+Then("Eu vejo uma tela com a mensagem de sucesso e os dados do endereco.") do
+  page.has_no_content?('Endereco was successfully created.')
+end
+
+
