bbfd251b9486ab2ce7035f88c204974a25c38f8a
alessandrojr1998 <alessandrojr1998@gmail.com>
2019-06-11 00:14:49 -0300

    teste de modelo do produto criado

--- barbeariaWeb/test/models/endereco_test.rb
+++ barbeariaWeb/test/models/endereco_test.rb
@@ -2,9 +2,7 @@ require 'test_helper'
 
 class EnderecoTest < ActiveSupport::TestCase
     test "Criando novo endereco" do
-      ende = Endereco.new(rua:'gabriel', bairro:'teste', numero:1, id:1)
-      assert_no ende.save
+      ende = Endereco.new(rua:"gabriel", bairro:"teste", numero:"1", id:1)
+      assert_not ende.save
     end
-  #   assert true
-  # end
 end
