3cbbe68056c202e904d081e93bd40444e2d9fa02
alessandrojr1998 <alessandrojr1998@gmail.com>
2019-06-11 00:14:49 -0300

    teste de modelo do produto criado

--- barbeariaWeb/test/models/barbearium_test.rb
+++ barbeariaWeb/test/models/barbearium_test.rb
@@ -2,9 +2,7 @@ require 'test_helper'
 
 class BarbeariumTest < ActiveSupport::TestCase
    test "Criando Barbearia" do
-     barb = Barbearium.new(nome:'Teste', contato:"123456789", id:1, endereco_id:1)
+     barb = Barbearium.new(nome:'Teste', contato:'123456789', id:1, endereco_id:1)
      assert_not barb.save
    end
-  #   assert true
-  # end
 end
