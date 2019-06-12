de58f2d175f5e75b43ebe6fcf7cf0cc4e2fe0ba3
AlvanirJr <alvanirjr95@gmail.com>
2019-06-11 23:35:49 -0300

    Novos testes de model criados

--- barbeariaWeb/test/models/produto_test.rb
+++ barbeariaWeb/test/models/produto_test.rb
@@ -7,4 +7,15 @@ class ProdutoTest < ActiveSupport::TestCase
     assert produto.save
   end
 
+  test "Criando Produto sem nome " do
+    produto = Produto.new(nome: "", descricao: "Secador de cabelos",
+                          valor: "12.00", quantidade: "10", barbearium_id: 1)
+    assert_not produto.save
+  end
+
+  test "Criando Produto sem preco " do
+    produto = Produto.new(nome: "Teste", descricao: "Secador de cabelos",
+                          valor: "", quantidade: "10", barbearium_id: 1)
+    assert_not produto.save
+  end
 end
--- barbeariaWeb/test/models/usuario_test.rb
+++ barbeariaWeb/test/models/usuario_test.rb
@@ -5,4 +5,13 @@ class UsuarioTest < ActiveSupport::TestCase
      user = Usuario.new(sobrenome:"Teste", senha:"123456789", cpf:"12345678900", contato:"12345678")
     assert_not user.save
    end
+
+   test 'usuario com sobrenome vazio' do
+     user = Usuario.new(sobrenome:"", senha:"123456789", cpf:"12345678900", contato:"12345678")
+     assert_not user.save
+   end
+   test 'usuario com cpf vazio' do
+     user = Usuario.new(nome: "Teste" ,sobrenome:"nome", senha:"123456789", cpf:" ", contato:"12345678")
+     assert_not user.save
+   end
 end
